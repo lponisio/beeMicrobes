5ffbf9c32083aaeeb97e97212bd9b350aade3234
Lauren Ponisio <lponisio@gmail.com>
2021-09-13 14:41:21 -0700

    re-named files and updated main to reflect those changes

--- 0main.sh
+++ 0main.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
 
 ## the entire wild bee community
-Rscript 1SiteComposition.R 
-Rscript 2IndividualComp.R 
-Rscript 3FuncModsBayes.R 
+Rscript 1IndividualComp.R 
+Rscript 2TraitMods.R 
 Rscript 4ModelChecks.R 
 
